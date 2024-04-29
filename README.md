087f8d23df62114428c444a69d13773f9ffd93c0
TuTruc37 <lychenhtutruc2003@gmail.com>
2024-04-29 15:12:13 +0700

    update nho xem readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+Một số thư viện sử dụng trong dự án
+
+- React-router-dom (Routing)
+- Axios (Kết nối gọi dữ liệu backend)
+- Redux-toolkit (Global State)
+- Sass (Hỗ trợ viết css) npm i sass
+- Tailwind css (Ultility class)
+- Antd (Component)
+- Formik và Yup (Quản lí Form)
+- lottie-react (animation)
+- moment (chỉnh lại giờ ngày năm đẹp)
---
+++ src/services/config.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+export const http = axios.create({
+  baseURL: 'https://movienew.cybersoft.edu.vn/api',
+  headers: {
+    tokenCybersoft:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2MSIsIkhldEhhblN0cmluZyI6IjI4LzA5LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcyNzQ4MTYwMDAwMCIsIm5iZiI6MTY5ODUxMjQwMCwiZXhwIjoxNzI3NjI5MjAwfQ.uWn4XmIr3aGBNm4QCi5Q5RFxVqNTwws8-EDFxQQud_I',
+  },
+  timeout: 30000,
+});
