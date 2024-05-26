ced5d8daa43a294dc55454d6fdfb2e4473a16e0e
TuTruc37 <lychenhtutruc2003@gmail.com>
2024-05-26 12:36:04 +0700

    update lại inputCustom

--- src/components/Input/InputCustom.jsx
+++ src/components/Input/InputCustom.jsx
@@ -16,7 +16,7 @@ const InputCustom = ({
     <div className={className}>
       <label
         htmlFor="first_name"
-        className="block mb-2 text-sm font-medium text-white text-xl "
+        className="block mb-2 font-medium text-white text-lg "
       >
         {label}
       </label>
@@ -25,7 +25,7 @@ const InputCustom = ({
         onChange={handleChange}
         onBlur={handleBlur}
         type={type}
-        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 `}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 `}
         placeholder={placeholder}
         value={value}
       />
