a802d0aa87740a8cdded0d6a3ea5c24ce866bc85
TuTruc37 <lychenhtutruc2003@gmail.com>
2024-05-30 13:25:27 +0700

    update TableAdmin

--- src/pages/Films/FilmsManager/FilmsManager.jsx
+++ src/pages/Films/FilmsManager/FilmsManager.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import TableAdmin from '../../../layout/TableAdmin/TableAdmin';
+import { quanLyNguoiDungServ } from '../../../services/quanLyNguoiDungServ';
 const FilmsManager = () => {
+  const [arrFilms, setArrFilms] = useState([]);
+  useEffect(() => {
+    handleGetAllUser();
+  }, []);
+  const handleGetAllUser = () => {
+    quanLyNguoiDungServ
+      .layDanhSachNguoiDung()
+      .then(res => {
+        console.log(res);
+        setArrFilms(res.data.content);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <h1 className="text-2xl font-bold">Quản lý phim</h1>
       <button className="py-2 px-5 bg-blue-500 hover:bg-blue-700 text-white rounded">
         Thêm phim
       </button>
+      <TableAdmin arrFilms={arrFilms} />
     </div>
   );
 };
--- src/services/quanLyNguoiDungServ.js
+++ src/services/quanLyNguoiDungServ.js
@@ -8,4 +8,7 @@ export const quanLyNguoiDungServ = {
     console.log(data);
     return http.post('/QuanLyNguoiDung/DangKy', data);
   },
+  layDanhSachNguoiDung: () => {
+    return http.get('/QuanLyPhim/LayDanhSachPhim?maNhom=GP01');
+  },
 };
