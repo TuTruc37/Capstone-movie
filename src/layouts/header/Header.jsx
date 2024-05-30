f6133cfbc0927fa6ca4e0c949539e94a465a4955
Tuandev <thanhtuan.mail.dev@gmail.com>
2024-05-30 16:29:58 +0700

    update

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -72,12 +72,22 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a
+                 
+                  <div className="">
+                <span className="block text-sm text-gray-900 dark:text-white">
+                </span>
+                <a
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                   >
                     Sign out
+                  </a> <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  >
+                    Sign in
                   </a>
+              </div>
                 </li>
               </ul>
             </div>
@@ -110,7 +120,7 @@ const Header = () => {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-user"
           >
-            <ul className="flex bg-transparent flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex bg-transparent flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:border-gray-700">
               <li>
                 <a
                   href="#"
