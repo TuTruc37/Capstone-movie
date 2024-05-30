109c9e769108bf6de066f8272220f90b2b6f335c
Tuandev <thanhtuan.mail.dev@gmail.com>
2024-05-30 10:43:33 +0700

    update

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
+          <as
             href="https://flowbite.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
@@ -13,7 +13,7 @@ const Header = () => {
             <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white tracking-wider">
               IMAX
             </span>
-          </a>
+          </as>
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
