64c69af62180a876d3649bc5778cbefe82451646
Quynhnehe <149154411+Quynhnehe@users.noreply.github.com>
2024-05-31 10:01:25 +0700

    fix

--- src/component/HeThongLichCumRap/HeThongCumRap.jsx
+++ src/component/HeThongLichCumRap/HeThongCumRap.jsx
@@ -6,57 +6,117 @@ import '../HeThongLichCumRap/HeThongCumRap.scss'
 
 const HeThongCumRap = ({ maHeThongRap }) => {
   const [arrCumRap, setArrCumRap] = useState([]);
+  // useEffect(() => {
+  //   console.log(maHeThongRap)
+  //   quanLyRapSer
+  //     .layThongTinLichChieuHeThongRap(maHeThongRap)
+  //     .then((res) => {
+  //       console.log(res);
+  //       setArrCumRap(res.data.content[0].lstCumRap);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  // }, [maHeThongRap])
   useEffect(() => {
-    console.log(maHeThongRap)
-    quanLyRapSer
-      .layThongTinLichChieuHeThongRap(maHeThongRap)
-      .then((res) => {
-        console.log(res);
-        setArrCumRap(res.data.content[0].lstCumRap);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, [maHeThongRap])
+    if (idCinema) {
+      listCinema
+        .getListTimeShowTimes(idCinema)
+        .then(res => {
+          // console.log(res);
+          if (res.data && res.data.content && res.data.content.length > 0) {
+            setArrListCinema(res.data.content[0].lstCumRap);
+          } else {
+            console.error('Unexpected response format', res);
+          }
+        })
+        .catch(err => {
+          console.error('Error fetching cinema list', err);
+        });
+    }
+  }, [idCinema]);
+
 
   const [tabPosition, setTabPosition] = useState('left');
   const changeTabPosition = (e) => {
     setTabPosition(e.target.value);
   };
   return (
-    <div className='he_thong_cum_rap'>
+    <div>
       <Tabs
-        tabPosition="left"
-        style={{
-          height: '600px',
-        }}
-        items={arrCumRap.map((cumRap, index) => {
-          // console.log(cumRap.content[0].danhSachPhim);
-          return {
-            label: (
-              <div key={index} className='text-left w-[250px] mx-1'>
-                <h3 className='uppercase text-green-600 text-lg font-medium truncate'>{cumRap.tenCumRap}</h3>
-                <p className='truncate text-xs'>{cumRap.diaChi}</p>
-              </div>
-            ),
-            key: index,
-            children: (
-              <div>
-                <div>
-                  <img src='' alt="" />
-                </div>
-                <div>
-                  <h1>Ten Phim</h1>
-
-                </div>
-              </div>
-            )
-          
-          };
-        })}
+        tabPosition="top"
+        items={arrListCinema.map((item, index) => ({
+          label: (
+            <div className="list-cinema-content" key={index}>
+              <h3 className="text-red-500 list-cinema-heading font-medium text-2xl">
+                {item.tenCumRap}
+              </h3>
+              <p className="text-white text-lg list-cinema-title truncate w-[600px]">
+                {item.diaChi}
+              </p>
+            </div>
+          ),
+          key: index,
+          children: (
+            <div className=" list-cinema grid grid-cols-3 space-x-5 overflow-y-scroll h-[600px]">
+              {item.danhSachPhim.map((item, index) => {
+                return (
+                  item.dangChieu && (
+                    <div key={index} className="flex my-5 ">
+                      <div className="mr-5">
+                        <img
+                          className="h-40 w-40 object-cover  overflow-hidden"
+                          src={item.hinhAnh}
+                          alt=""
+                        />
+                      </div>
+                      <div className="">
+                        <h3 className="flex items-center space-x-2">
+                          <span className="text-white px-5 py-1 rounded font-semibold bg-red-500">
+                            C18
+                          </span>
+                          <span className="text-white flex text-xl font-bold">
+                            {item.tenPhim}
+                          </span>
+                        </h3>
+                        <div className="list-cinema-bottom mt-3 grid grid-cols-2 gap-5">
+                          {item.lstLichChieuTheoPhim
+                            .slice(0, 4)
+                            .map((item, index) => {
+                              return (
+                                <div
+                                  className="text-white list-cinema-content-bottom "
+                                  key={index}
+                                >
+                                  <div className=" flex list-cinema-time text-md font-semibold py-2  px-4 rounded whitespace-nowrap  bg-slate-500">
+                                    {moment(item.ngayChieuGioChieu).format(
+                                      ' DD-MM-YYYY '
+                                    )}
+                                   <div className='ml-1 list-cinema-time-pc'>
+                                   {moment().format( " ~ hh:mm ")}
+                                   </div>
+                                  </div>
+                                  <div className="  flex list-cinema-number text-lg font-semibold">
+                                    {item.tenRap} 
+                                    <div className=' list-cinema-time-mobile   font-semibold'>
+                                      {moment().format(" - hh:mm")}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            })}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+            </div>
+          ),
+        }))}
       />
     </div>
-  )
+  );
 }
 
 export default HeThongCumRap
