f73796737101dc9aa270085bb30b7f3f92d65251
Tuandev <thanhtuan.mail.dev@gmail.com>
2024-05-31 15:13:00 +0700

    update

--- src/layouts/TableAdmin/TableAdmin.jsx
+++ src/layouts/TableAdmin/TableAdmin.jsx
@@ -1,29 +1,27 @@
 import { Table } from 'antd';
 import React, { useContext } from 'react';
 import { AlertContext } from '../../App';
+
 const TableAdmin = ({ arrFilms }) => {
-  console.log(arrFilms);
   const { handleAlert } = useContext(AlertContext);
+
   const columns = [
     {
       title: 'Mã phim',
       dataIndex: 'maPhim',
-      sorter: {
-        compare: (a, b) => a.maPhim - b.maPhim,
-        multiple: 1,
-      },
+      sorter: (a, b) => a.maPhim - b.maPhim,
     },
     {
       title: 'Hình ảnh',
       dataIndex: 'hinhAnh',
+      render: hinhAnh => (
+        <img src={hinhAnh} alt="Hình ảnh" className='w-[900px]' />
+      ),
     },
     {
       title: 'Tên phim',
       dataIndex: 'tenPhim',
-      sorter: {
-        compare: (a, b) => a.tenPhim - b.tenPhim,
-        multiple: 1,
-      },
+      sorter: (a, b) => a.tenPhim.localeCompare(b.tenPhim),
     },
     {
       title: 'Mô tả',
@@ -32,34 +30,30 @@ const TableAdmin = ({ arrFilms }) => {
     {
       title: 'Chức năng',
       dataIndex: 'taiKhoan',
-      render: (text, record) => {
-        // console.log(record);
-        return (
-          <>
-          <div className='flex'>
+      render: (text, record) => (
+        <div className="flex">
           <button className="py-2 px-4 rounded text-white bg-yellow-300 mr-3">
-              <i className="fa-solid fa-pen"></i>
-            </button>
-            <button className="py-2 px-4 rounded text-white bg-red-500 mr-3">
-              <i className="fa-solid fa-trash"></i>
-            </button>
-          </div>
-          </>
-        );
-      },
+            <i className="fa-solid fa-pen"></i>
+          </button>
+          <button className="py-2 px-4 rounded text-white bg-red-500 mr-3">
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      ),
     },
   ];
+
   const onChange = (pagination, filters, sorter, extra) => {
     console.log('params', pagination, filters, sorter, extra);
   };
+
   return (
     <div>
       <Table
         columns={columns}
         dataSource={arrFilms}
-        pagination={{
-          defaultPageSize: 20,
-        }}
+        rowKey="maPhim"
+        pagination={{ defaultPageSize: 20 }}
         onChange={onChange}
       />
     </div>
