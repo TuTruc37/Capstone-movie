e57cb56933168db97f97a2ac9494270b775d0234
AnthonyNguyen98 <nmTan2009@gmail.com>
2024-06-02 00:20:30 +0700

    -

--- src/services/quanLyPhongVe.js
+++ src/services/quanLyPhongVe.js
@@ -4,4 +4,8 @@ export const chiTietPhongVe = {
     thongTinLichChieu: (maLichChieu) => {
         return http.get('/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=' + maLichChieu);
     },
+    
+    datVe: (data) => {
+        return http.post('/QuanLyDatVe/DatVe', data);
+    },
 }
