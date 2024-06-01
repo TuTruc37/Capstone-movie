4680c4db2aa2860ee7e7871a81756b3f5910cc14
AnthonyNguyen98 <nmTan2009@gmail.com>
2024-06-02 00:20:30 +0700

    add: responsive for mobile

--- src/templates/ChiTietPhongVe.jsx
+++ src/templates/ChiTietPhongVe.jsx
@@ -25,7 +25,7 @@ const ChiTietPhongVe = () => {
     if (dataUser == null) {
       handleAlert('error', 'Bạn cần đăng nhập để đặt vé');
       navigate(path.dangNhap)
-    } 
+    }
 
     chiTietPhongVe.thongTinLichChieu(maLichChieu).then(res => {
       setData(res.data);
@@ -83,43 +83,43 @@ const ChiTietPhongVe = () => {
       <Header />
 
       <div className="flex justify-center mb-10 mt-16">
-        <div className="w-2/3 flex">
-          <div className="w-2/3">
-            <div>
-              <div className="flex space-x-5 mt-10">
-                <div align="center">
-                  <div className="bg-slate-100 w-[40px] h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
-                  Ghế thường
-                </div>
+        <div className="w-full lg:flex justify-center space-x-5">
+          <div className="flex justify-center">
+            <div className="w-3/4 mx-2 lg:w-full">
+              <div>
+                <div className="flex justify-center space-x-5 mt-10">
+                  <div align="center">
+                    <div className="bg-slate-100 w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
+                    Ghế thường
+                  </div>
 
-                <div align="center">
-                  <div className="bg-orange-400 w-[40px] h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
-                  Ghế Vip
+                  <div align="center">
+                    <div className="bg-orange-400 w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
+                    Ghế Vip
+                  </div>
+                  <div align="center">
+                    <div className="bg-green-400 w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
+                    Ghế Đang Chọn
+                  </div>
+                  <div align="center">
+                    <div className="bg-red-400 w-[20px] h-[20px] lg:w-[40px] lg:h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
+                    Ghế Đã Chọn
+                  </div>
                 </div>
-                <div align="center">
-                  <div className="bg-green-400 w-[40px] h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
-                  Ghế Đang Chọn
-                </div>
-                <div align="center">
-                  <div className="bg-red-400 w-[40px] h-[40px] rounded-md flex items-center justify-center mb-2"></div>{' '}
-                  Ghế Đã Chọn
-                </div>
+                <img src={screenImg} className="mt-10 mx-auto" />
               </div>
-              <img src={screenImg} className="mt-10 mx-auto" />
-
               <div className="mt-10 grid grid-cols-12 gap-4">
                 {data?.content?.danhSachGhe?.map(ghe => (
                   <button
                     key={ghe.stt}
-                    className={`${
-                      ghe.daDat === true
-                        ? 'bg-red-400'
-                        : gheDaChon.includes(ghe.stt)
+                    className={`${ghe.daDat === true
+                      ? 'bg-red-400'
+                      : gheDaChon.includes(ghe.stt)
                         ? 'bg-green-400'
                         : ghe.loaiGhe === 'Vip'
-                        ? 'bg-orange-400'
-                        : 'bg-slate-100'
-                    } w-[40px] h-[40px] rounded-md flex items-center justify-center`}
+                          ? 'bg-orange-400'
+                          : 'bg-slate-100'
+                      } w-[25px] h-[25px] lg:w-[40px] lg:h-[40px] rounded-md flex items-center justify-center text-xs`}
                     onClick={() => chonGhe(ghe.stt)}
                     disabled={ghe.daDat}
                   >
@@ -130,53 +130,56 @@ const ChiTietPhongVe = () => {
             </div>
           </div>
 
-          <div className="w-1/3 bg-slate-100 rounded-lg p-5 mt-16">
-            <h1 className="flex justify-center font-bold mb-10">
-              {data?.content?.thongTinPhim?.tenPhim}
-            </h1>
-            <div className="flex justify-between items-center border-t-2 border-b-2 border-dashed p-2">
-              <div>
-                Ngày chiếu - Giờ Chiếu: {data?.content?.thongTinPhim?.ngayChieu}
-                :
+          <div className="flex justify-center">
+            <div className="w-full lg:w-full lg:mx-0 bg-slate-100 rounded-lg p-5 mt-16">
+              <h1 className="flex justify-center font-bold mb-10">
+                {data?.content?.thongTinPhim?.tenPhim}
+              </h1>
+              <div className="flex justify-between items-center border-t-2 border-b-2 border-dashed p-2">
+                <div>
+                  Ngày chiếu - Giờ Chiếu: {data?.content?.thongTinPhim?.ngayChieu}
+                  :
+                </div>
+                <p className="text-orange-400">
+                  {data?.content?.thongTinPhim?.gioChieu}
+                </p>
               </div>
-              <p className="text-orange-400">
-                {data?.content?.thongTinPhim?.gioChieu}
-              </p>
-            </div>
-            <div className="flex justify-between items-center border-b-2 border-dashed p-2">
-              <div>Cụm rạp: </div>
-              <p className="text-orange-400">
-                {data?.content?.thongTinPhim?.tenCumRap}
-              </p>
-            </div>
-            <div className="flex justify-between items-center border-b-2 border-dashed p-2">
-              <div>Rạp:</div>
-              <p className="text-orange-400">
-                {data?.content?.thongTinPhim?.tenRap}
-              </p>
-            </div>
-            <div className="flex justify-between items-center border-b-2 border-dashed p-2">
-              <div>Ghế Chọn:</div>
-              <p className="text-orange-400">{danhSachGheDaChon.join(', ')}</p>
-            </div>
-            <div className="flex justify-between items-center border-b-2 border-dashed p-2">
-              <div>Ưu đãi:</div>
-              <p className="text-orange-400">0%</p>
-            </div>
-            <div className="flex justify-between items-center border-b-2 border-dashed p-2">
-              <div>Tổng tiền:</div>
-              <p className="text-orange-400">
-                {new Intl.NumberFormat('vi-VN', {
-                  style: 'currency',
-                  currency: 'VND',
-                }).format(total)}
-              </p>
-            </div>
+              <div className="flex justify-between items-center border-b-2 border-dashed p-2">
+                <div>Cụm rạp: </div>
+                <p className="text-orange-400">
+                  {data?.content?.thongTinPhim?.tenCumRap}
+                </p>
+              </div>
+              <div className="flex justify-between items-center border-b-2 border-dashed p-2">
+                <div>Rạp:</div>
+                <p className="text-orange-400">
+                  {data?.content?.thongTinPhim?.tenRap}
+                </p>
+              </div>
+              <div className="flex justify-between items-center border-b-2 border-dashed p-2">
+                <div>Ghế Chọn:</div>
+                <p className="text-orange-400">{danhSachGheDaChon.join(', ')}</p>
+              </div>
+              <div className="flex justify-between items-center border-b-2 border-dashed p-2">
+                <div>Ưu đãi:</div>
+                <p className="text-orange-400">0%</p>
+              </div>
+              <div className="flex justify-between items-center border-b-2 border-dashed p-2">
+                <div>Tổng tiền:</div>
+                <p className="text-orange-400">
+                  {new Intl.NumberFormat('vi-VN', {
+                    style: 'currency',
+                    currency: 'VND',
+                  }).format(total)}
+                </p>
+              </div>
 
-            <button className="w-full bg-orange-400 text-white rounded-md px-5 py-2 mt-4" onClick={() => datVe()}>
-              Đồng Ý
-            </button>
+              <button className="w-full bg-orange-400 text-white rounded-md px-5 py-2 mt-4" onClick={() => datVe()}>
+                Đồng Ý
+              </button>
+            </div>
           </div>
+
         </div>
       </div>
 
